ea6b70011ad0ccf6f2d85c51825f8b00bab745eb
Cameron-Stuart <cameronstuart861@gmail.com>
2020-08-28 12:56:25 +0100

    Added Hello World

---
+++ Hello World/Program.fs
@@ -0,0 +1,7 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World!"
+    0 // return an integer exit code
