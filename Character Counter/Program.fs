2a78420df911e7ef13fb409a375bda44279ea859
Cameron-Stuart <cameronstuart861@gmail.com>
2020-08-28 14:10:24 +0100

    Added character counter project - Now in .NET Core

---
+++ Character Counter/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let countLength string = String.length string
+
+[<EntryPoint>]
+let main argv =
+    printfn "Enter a string to have the length calculated!"
+    let i = Console.ReadLine()
+    printfn "Length: %i" (countLength i)
+    0 // return an integer exit code
